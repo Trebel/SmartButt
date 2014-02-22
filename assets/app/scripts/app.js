ca582aa7b49049f78e1dcbd4c973af2f67a5e7b6
David King <davidgwking@gmail.com>
2014-02-22 17:13:46 -0600

    scheduled ajax request for loos

--- assets/app/scripts/app.js
+++ assets/app/scripts/app.js
@@ -1,6 +1,6 @@
 angular.module('app', [])
 
-.controller('controller', function($scope, $http) {
+.controller('controller', function($scope, $http, $interval) {
   $http({
     url: "/api/toilets",
     method: "GET",
@@ -12,4 +12,19 @@ angular.module('app', [])
   }).error(function(error){
       $scope.error = error;
   });
+
+  $interval(
+    function () {
+      $http({
+        url: "/api/toilets",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+      }).success(function(response){
+          $scope.loos = response;
+      }).error(function(error){
+          $scope.error = error;
+      });
+    }, 5000);
 });
