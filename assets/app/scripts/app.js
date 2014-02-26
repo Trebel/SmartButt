7b885234ef512ae902f891ca173111701bed1158
ryyan <test@test.com>
2014-02-26 17:43:21 -0600

    use mock data if the api is borked

--- assets/app/scripts/app.js
+++ assets/app/scripts/app.js
@@ -10,7 +10,34 @@ angular.module('app', [])
   }).success(function(response){
       $scope.loos = response;
   }).error(function(error){
-      $scope.error = error;
+   $scope.loos = [
+      {
+        "id":"3910360",
+        "floor":"1",
+        "bathroom":"2",
+        "stall":"2",
+        "occupied":false,
+        "occupyChance":19.84247250016779,
+        "tags":[""]
+      },
+      {
+        "id":"234233",
+        "floor":"3",
+        "bathroom":"1",
+        "stall":"1",
+        "occupied":false,
+        "occupyChance":50.86311145219952,
+        "tags":["handicap"]
+      },
+      {
+        "id":"3910361",
+        "floor":"2",
+        "bathroom":"1",
+        "stall":"1",
+        "occupied":true,
+        "occupyChance":83.86311145219952,
+        "tags":["handicap","private"]
+      }];
   });
 
   $interval(
